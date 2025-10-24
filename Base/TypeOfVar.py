934cdb8b2738888ad77a92ea2896205dcf3ac7ad
1175476542 <1175476542@qq.com>
2025-10-24 23:56:57 +0800

    variable to learn

---
+++ Base/TypeOfVar.py
@@ -0,0 +1,23 @@
+num = 1
+num1 = 1.1
+# type(num)
+print(type(num))
+print(type(num1))
+
+str = "string"
+print(type(str))
+
+bool = True
+print(type(bool))
+
+lis = [1,2,3,4]
+print(type(lis))
+tup = (1,2,3,4)
+print(type(tup))
+
+set1 = {1,2,3,4,5,6,7,8,9,10}
+print(type(set1))
+
+
+dic = {'name':'Tom','age':18}
+print(type(dic))
---
+++ Base/VarTest.py
@@ -0,0 +1,5 @@
+x = 1
+print(x)
+name = 'Tom'
+print(name)
+# print(name+x)
